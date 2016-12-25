5494deb31efa97c931b9ed068ee47e652035edaa
gayanjusto <gayan.justo@gmail.com>
2016-12-24 23:34:52 -0200

    Initial Commit

---
+++ IoCContainer/IoC/Container.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IoCContainer.IoC
+{
+    public enum IoCLifeCycle
+    {
+        Singleton = 1,
+    }
+    public class Container
+    {
+
+        private Dictionary<Type, Type> typeToImplementation;
+        private Dictionary<Type, object> singletonInstances;
+
+
+        public Container()
+        {
+            typeToImplementation = new Dictionary<Type, Type>();
+            singletonInstances = new Dictionary<Type, object>();
+        }
+
+        public void Register<TInterface, TImplementation>()
+        {
+            typeToImplementation[typeof(TInterface)] = typeof(TImplementation);
+        }
+
+        public void Register<TInterface, TImplementation>(IoCLifeCycle lifeCycle)
+        {
+            typeToImplementation[typeof(TInterface)] = typeof(TImplementation);
+
+            if (lifeCycle == IoCLifeCycle.Singleton)
+            {
+                singletonInstances[typeof(TInterface)] = Activator.CreateInstance(typeToImplementation[typeof(TInterface)]);
+            }
+        }
+
+        public TInterface GetImplementation<TInterface>()
+        {
+            if (singletonInstances.ContainsKey(typeof(TInterface)))
+            {
+                return (TInterface)singletonInstances[typeof(TInterface)];
+            }
+
+            return (TInterface)Activator.CreateInstance(typeToImplementation[typeof(TInterface)]);
+        }
+
+        public object Resolve(Type contract)
+        {
+            if (typeToImplementation.ContainsKey(contract))
+            {
+                return typeToImplementation[contract];
+            }
+            return null;
+        }
+    }
+}
---
+++ IoCContainer/Program.cs
@@ -0,0 +1,28 @@
+﻿using IoCContainer.IoC;
+
+namespace IoCContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new Container();
+
+            container.Register<ITest, Test>(IoCLifeCycle.Singleton);
+
+            var testeImp = container.GetImplementation<ITest>();
+        }
+    }
+
+    public interface ITest
+    {
+
+    }
+
+    public class Test : ITest
+    {
+        public int Nome { get; set; }
+        public int Idade { get; set; }
+
+    }
+}
